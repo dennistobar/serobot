3136b48917dd48a0d1222646b573053a7c21c105
dennistobar <dennis.tobar@gmail.com>
2018-08-28 17:38:31 -0300

    Añadiendo opción de más wikis

--- main.py
+++ main.py
@@ -19,6 +19,7 @@ class SeroBOT(Bot):
         self.availableOptions.update({
             'gf': 0.085,
             'dm': 0.970,
+            'wiki': 'eswiki',
             'debug': False
         })
         """Constructor."""
@@ -46,15 +47,17 @@ class SeroBOT(Bot):
         return page._rcinfo.get('type') == 'edit' and page._rcinfo.get('bot') == False and page._rcinfo.get('namespace') in [0, 104] and page._rcinfo.get('user') != self.site.username()
 
     def checkORES(self, page):
+        wiki = self.getOption('wiki')
         revision = page._rcinfo.get('revision')
         ores = str(revision.get('new'))
-        url = 'https://ores.wmflabs.org/v3/scores/eswiki/' + ores
+        url = 'https://ores.wmflabs.org/v3/scores/{0}/{1}'.format(wiki, ores)
+        print (url)
         retornar = {}
         r = requests.get(url=url)
         data = r.json()
         try:
-            goodfaith = data.get('eswiki').get('scores').get(ores).get('goodfaith')
-            damaging = data.get('eswiki').get('scores').get(ores).get('damaging')
+            goodfaith = data.get(wiki).get('scores').get(ores).get('goodfaith')
+            damaging = data.get(wiki).get('scores').get(ores).get('damaging')
             buena_fe = goodfaith.get('score').get('probability').get('true')
             danina = damaging.get('score').get('probability').get('true')
             return (ores, buena_fe, danina, True if buena_fe < self.getOption('gf') or danina > self.getOption('dm') else False)
@@ -76,30 +79,31 @@ class SeroBOT(Bot):
         past = (int(datetime.utcnow().timestamp()) - df_reversas[7]) < (60*60*4) # 4 horas
         rows = df_reversas[user & page & past]
         User = pywikibot.User(self.site, usuario)
-        if (len(rows) == 2 and User.isAnonymous() == False):
-            if self.getOption('debug'):
-                print ('Avisando a ',usuario)
-            talk = pywikibot.Page(self.site, title=usuario, ns=3)
-            talk.text += u"\n{{sust:Aviso prueba2|"+ pagina +"}} ~~~~"
-            talk.save(comment=u'Aviso de pruebas a usuario tras reversiones consecutivas')
-            with open(os.path.dirname(os.path.realpath(__file__)) + '/log/discusiones.log', 'a+') as archivo:
-                archivo.write('\t'.join(map(lambda x: str(x), [usuario, pagina, datetime.utcnow().strftime('%Y%m%d%H%M%S'), int(datetime.utcnow().timestamp())])) + '\n')
-            return
-        rows = df_reversas[user & past]
-        if (len(rows) == 4):
-            if self.getOption('debug'):
-                print ('VEC a ',usuario)
-            with open(os.path.dirname(os.path.realpath(__file__)) + '/log/vec.log', 'a+') as archivo:
-                archivo.write('\t'.join(map(lambda x: str(x), [usuario, datetime.utcnow().strftime('%Y%m%d%H%M%S'), int(datetime.utcnow().timestamp())])) + '\n')
+        if ((self.site.family.name == 'wikipedia' and self.site.lang == 'es')):
+            if (len(rows) == 2 and User.isAnonymous() == False):
+                if self.getOption('debug'):
+                    print ('Avisando a ',usuario)
+                talk = pywikibot.Page(self.site, title=usuario, ns=3)
+                talk.text += u"\n{{sust:Aviso prueba2|"+ pagina +"}} ~~~~"
+                talk.save(comment=u'Aviso de pruebas a usuario tras reversiones consecutivas')
+                with open(os.path.dirname(os.path.realpath(__file__)) + '/log/discusiones.log', 'a+') as archivo:
+                    archivo.write('\t'.join(map(lambda x: str(x), [usuario, pagina, datetime.utcnow().strftime('%Y%m%d%H%M%S'), int(datetime.utcnow().timestamp())])) + '\n')
+                return
+            rows = df_reversas[user & past]
+            if (len(rows) == 4):
+                if self.getOption('debug'):
+                    print ('VEC a ',usuario)
+                with open(os.path.dirname(os.path.realpath(__file__)) + '/log/vec.log', 'a+') as archivo:
+                    archivo.write('\t'.join(map(lambda x: str(x), [usuario, datetime.utcnow().strftime('%Y%m%d%H%M%S'), int(datetime.utcnow().timestamp())])) + '\n')
 
-            vec = pywikibot.Page(self.site, title='Vandalismo en curso', ns=4)
-            tpl = u'{{subst:'
-            tpl += 'ReportevandalismoIP' if User.isAnonymous() else 'Reportevandalismo'
-            tpl += u'|1='+usuario
-            tpl += u'|2=Reversiones: '+(', '.join(map(lambda x: u'[[Special:Diff/'+str(x)+'|diff: '+str(x)+']]', rows[0])))
-            tpl += u'}}'
-            vec.text += "\n"+tpl
-            vec.save(comment=u'Reportando al usuario [[Special:Contributions/'+usuario+'|'+usuario+']] por posibles reversiones vandálicas')
+                vec = pywikibot.Page(self.site, title='Vandalismo en curso', ns=4)
+                tpl = u'{{subst:'
+                tpl += 'ReportevandalismoIP' if User.isAnonymous() else 'Reportevandalismo'
+                tpl += u'|1='+usuario
+                tpl += u'|2=Reversiones: '+(', '.join(map(lambda x: u'[[Special:Diff/'+str(x)+'|diff: '+str(x)+']]', rows[0])))
+                tpl += u'}}'
+                vec.text += "\n"+tpl
+                vec.save(comment=u'Reportando al usuario [[Special:Contributions/'+usuario+'|'+usuario+']] por posibles reversiones vandálicas')
             return
 
     def do_reverse(self, page):
@@ -138,13 +142,20 @@ def main(*args):
             opts['gf'] = float(arg[4:])
         elif arg.startswith('-dm:'):
             opts['dm'] = float(arg[4:])
+        elif arg.startswith('-wiki:'):
+            opts['wiki'] = arg[6:]
         elif arg.startswith('--debug'):
             opts['debug'] = True
 
-    bot = SeroBOT(pagegenerators.LiveRCPageGenerator(),
-                  site=pywikibot.Site(), **opts)
-    bot.run()
+    site = pywikibot.Site()
+    if 'wiki' in opts and opts['wiki'] != 'eswiki':
+        lang = opts['wiki'][0:2]
+        family = opts['wiki'][2:]
+        site = pywikibot.Site(lang, family)
 
+    bot = SeroBOT(pagegenerators.LiveRCPageGenerator(site),
+                  site=site, **opts)
+    bot.run()
 
 if __name__ == '__main__':
     main()
