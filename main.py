a98e37086e780281537563e21f5bb4a09d24597f
Dennis Tobar <dennis.tobar@gmail.com>
2023-04-20 16:19:42 -0400

    feat: cleanup and improve
    - Using pandas to clean the log file
    - Using default rollback function
    - Deleting debug options
    - Change == to "is"
    - Clean some logic to avoid use ternary conditions
    - pep8 in the list

--- main.py
+++ main.py
@@ -16,14 +16,13 @@ class SeroBOT(Bot):
         self.available_options.update({
             'gf': 0.085,
             'dm': 0.970,
-            'wiki': 'eswiki',
-            'debug': False
+            'wiki': 'eswiki'
         })
 
         super(SeroBOT, self).__init__(**kwargs)
         self.generator = generator
         self.site = site
-        if self.site.logged_in() == False:
+        if self.site.logged_in() is False:
             self.site.login()
         self.wiki = "{}{}".format(self.site.lang, str(
             self.site.family).replace('pedia', ''))
@@ -37,14 +36,12 @@ class SeroBOT(Bot):
                 continue
             data = [revision, buena_fe, danina, resultado,
                     page._rcinfo.get('user'), page.title(), datetime.utcnow().strftime('%Y%m%d%H%M%S'), int(datetime.utcnow().timestamp())]
-            if self.available_options.get('debug'):
-                print(data)
             self.do_log(data)
-            self.do_reverse(page) if resultado == True else False
-            if (self.site.family.name == 'wikipedia' and self.site.lang == 'es'):
-                self.check_user(page._rcinfo.get('user'),
-                                page.title()) if resultado == True else False
-                self.check_pagina(page.title()) if resultado == True else False
+            if resultado is True:
+                self.do_reverse(page)
+                if (self.site.family.name == 'wikipedia' and self.site.lang == 'es'):
+                    self.check_user(page._rcinfo.get('user'), page.title())
+                    self.check_pagina(page.title())
 
     def valid(self, page):
         """
@@ -58,7 +55,7 @@ class SeroBOT(Bot):
         @param page: página a chequear
         @return true
         """
-        return page._rcinfo.get('type') == 'edit' and page._rcinfo.get('bot') == False and page._rcinfo.get('namespace') in [0, 104] and page._rcinfo.get('user') != self.site.username()
+        return page._rcinfo.get('type') == 'edit' and page._rcinfo.get('bot') is False and page._rcinfo.get('namespace') in [0, 104] and page._rcinfo.get('user') != self.site.username()
 
     def checkORES(self, page):
         headers = {
@@ -87,9 +84,26 @@ class SeroBOT(Bot):
             os.path.realpath(__file__)), wiki)
         with open(general, encoding='utf-8', mode='a+') as archivo:
             archivo.write(u'\t'.join(map(lambda x: str(x), data)) + u'\n')
-        if data[3] == True:
+        if data[3] is True:
             with open(positivo, encoding='utf-8', mode='a+') as archivo:
                 archivo.write(u'\t'.join(map(lambda x: str(x), data)) + u'\n')
+
+        # remove old log
+        df = pd.read_csv(general, header=None, delimiter='\t')
+        df['time'] = pd.to_datetime(df[6], format='%Y%m%d%H%M%S', utc=True)
+        df['isToDelete'] = pd.to_datetime(
+            'now', utc=True) > df['time'] + pd.to_timedelta(6, unit='H')
+        df = df[df['isToDelete'] == False]
+        df.drop(columns=['isToDelete', 'time'], inplace=True)
+        df.to_csv(general, index=False, sep='\t', header=False)
+        # remove old log
+        df = pd.read_csv(positivo, header=None, delimiter='\t')
+        df['time'] = pd.to_datetime(df[6], format='%Y%m%d%H%M%S', utc=True)
+        df['isToDelete'] = pd.to_datetime(
+            'now', utc=True) > df['time'] + pd.to_timedelta(6, unit='H')
+        df = df[df['isToDelete'] == False]
+        df.drop(columns=['isToDelete', 'time'], inplace=True)
+        df.to_csv(positivo, index=False, sep='\t', header=False)
 
     def check_user(self, usuario, pagina):
         wiki = self.wiki
@@ -98,41 +112,30 @@ class SeroBOT(Bot):
         df_reversas = pd.read_csv(positivo, header=None, delimiter='\t')
         user = df_reversas[4] == usuario
         page = df_reversas[5] == pagina
-        past = (int(datetime.utcnow().timestamp()) -
-                df_reversas[7]) < (60*60*4)  # 4 horas
+        past = (int(datetime.utcnow().timestamp())
+                - df_reversas[7]) < (60 * 60 * 4)  # 4 horas
         rows = df_reversas[user & page & past]
         User = pywikibot.User(self.site, usuario)
-        if (len(rows) == 2 and User.isAnonymous() == False):
-            if self.available_options.get('debug'):
-                print('Avisando a ', usuario)
+        if (len(rows) == 2 and User.isAnonymous() is False):
             talk = pywikibot.Page(self.site, title=usuario, ns=3)
             talk.text += u"\n{{sust:Aviso prueba2|" + pagina + "}} ~~~~"
             talk.save(
                 summary=u'Aviso de pruebas a usuario tras reversiones consecutivas')
-            with open(os.path.dirname(os.path.realpath(__file__)) + '/log/discusiones.log', 'a+') as archivo:
-                archivo.write('\t'.join(map(lambda x: str(x), [usuario, pagina, datetime.utcnow(
-                ).strftime('%Y%m%d%H%M%S'), int(datetime.utcnow().timestamp())])) + '\n')
             return
         rows = df_reversas[user & past]
         if (len(rows) == 4):
-            if self.available_options.get('debug'):
-                print('VEC a ', usuario)
-            with open(os.path.dirname(os.path.realpath(__file__)) + '/log/vec.log', 'a+') as archivo:
-                archivo.write('\t'.join(map(lambda x: str(x), [usuario, datetime.utcnow(
-                ).strftime('%Y%m%d%H%M%S'), int(datetime.utcnow().timestamp())])) + '\n')
-
             vec = pywikibot.Page(self.site, title='Vandalismo en curso', ns=4)
-            tpl = "\n"+u'{{subst:'
+            tpl = "\n" + u'{{subst:'
             tpl += 'ReportevandalismoIP' if User.isAnonymous() else 'Reportevandalismo'
-            tpl += u'|1='+usuario
+            tpl += u'|1=' + usuario
             tpl += u'|2=Reversiones: ' + \
                 (', '.join(
-                    map(lambda x: u'[[Special:Diff/'+str(x)+'|diff: '+str(x)+']]', rows[0])))
+                    map(lambda x: u'[[Special:Diff/' + str(x) + '|diff: ' + str(x) + ']]', rows[0])))
             tpl += u'}}'
-            vec.text += "\n"+tpl
+            vec.text += "\n" + tpl
             try:
-                vec.save(summary=u'Reportando al usuario [[Special:Contributions/' +
-                         usuario+'|'+usuario+']] por posibles ediciones vándalicas')
+                vec.save(summary=u'Reportando al usuario [[Special:Contributions/'
+                         + usuario + '|' + usuario + ']] por posibles ediciones vándalicas')
             except:
                 pass
         return
@@ -143,23 +146,21 @@ class SeroBOT(Bot):
             os.path.realpath(__file__)), wiki)
         df_reversas = pd.read_csv(positivo, header=None, delimiter='\t')
         page = df_reversas[5] == pagina
-        past = (int(datetime.utcnow().timestamp()) -
-                df_reversas[7]) < (60*60*4)  # 4 horas
+        past = (int(datetime.utcnow().timestamp())
+                - df_reversas[7]) < (60 * 60 * 4)  # 4 horas
         users = df_reversas[page & past][4].nunique()
 
         rows = df_reversas[page & past]
         if (len(rows) < 6 or users < 2):
             return
-        if self.available_options.get('debug'):
-            print('Solicitud de protección de página ', pagina)
 
         tabp = pywikibot.Page(
             self.site, title='Tablón de anuncios de los bibliotecarios/Portal/Archivo/Protección de artículos/Actual', ns=4)
         if (tabp.get().find('{{{{a|{0}}}}}'.format(pagina)) != -1):
             return
-        tpl = "\n"+'{{{{subst:Usuario:SeroBOT/TABP|pagina={0}|firma=~~~~}}}}'.format(
+        tpl = "\n" + '{{{{subst:Usuario:SeroBOT/TABP|pagina={0}|firma=~~~~}}}}'.format(
             pagina)
-        tabp.text += "\n"+tpl
+        tabp.text += "\n" + tpl
         try:
             tabp.save(
                 summary=u'Solicitando protección de [[{0}]] por reversiones consecutivas'.format(pagina))
@@ -168,20 +169,8 @@ class SeroBOT(Bot):
         return
 
     def do_reverse(self, page):
-        if self.available_options.get('debug'):
-            print('>> Reversion', page._rcinfo.get('user'), page.title())
         try:
-            token = pywikibot.data.api.Request(site=self.site, parameters={
-                                               'action': 'query', 'meta': 'tokens', 'type': 'rollback'}).submit()['query']['tokens']['rollbacktoken']
-
-            parameters = {'action': 'rollback',
-                          'title': page.title(),
-                          'user': page._rcinfo.get('user'),
-                          'token': token,
-                          'markbot': True}
-
-            pywikibot.data.api.Request(
-                site=self.site, parameters=parameters).submit()
+            self.site.rollbackpage(page)
             return
         except Exception as e:
             print(u'No puedo revertir la página ', e)
@@ -205,8 +194,6 @@ def main(*args):
             opts['dm'] = float(arg[4:])
         elif arg.startswith('-wiki:'):
             opts['wiki'] = arg[6:]
-        elif arg.startswith('--debug'):
-            opts['debug'] = True
 
     site = pywikibot.Site()
     if 'wiki' in opts and opts['wiki'] != 'eswiki':
